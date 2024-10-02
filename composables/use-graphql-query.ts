13e3822b8002368648d554eb5c5fcca4d6f1146e
Julien Reichel <julien.reichel@spotme.com>
2024-10-02 15:26:20 +0200

    fix: formatting

--- composables/use-graphql-query.ts
+++ composables/use-graphql-query.ts
@@ -1,52 +1,59 @@
-  /**
-  * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
-  */
+/**
+ * @type {import('aws-amplify/data').Client<import('../amplify/data/resource').Schema>}
+ */
 
-  // Define a generic type for the GraphQL query response
-  interface GraphQLResponse {
-    id: string;
-    [key: string]: string|object|number|undefined; // Allow other properties
-  }
+// Define a generic type for the GraphQL query response
+interface GraphQLResponse {
+  id: string;
+  [key: string]: string | object | number | undefined; // Allow other properties
+}
 
-  interface Model {
-    [key: string]: string|object|number|undefined; // Allow other properties
-  }
+interface Model {
+  [key: string]: string | object | number | undefined; // Allow other properties
+}
 
-  interface Options {
-    [key: string]: string|object|number|undefined; // Define specific options if needed
-  }
+interface Options {
+  [key: string]: string | object | number | undefined; // Define specific options if needed
+}
 
-  interface Params {
-    filter?: string;
-    limit?: number;
-    nextToken?: string;
-    sortDirection?: string;
-    [key: string]: string|object|number|undefined; // Define specific options if needed
-  }
+interface Params {
+  filter?: string;
+  limit?: number;
+  nextToken?: string;
+  sortDirection?: string;
+  [key: string]: string | object | number | undefined; // Define specific options if needed
+}
 
-  export default function (model:string, selectionSet:string[], listSelectionSet:string[]) {
-
+export default function (
+  model: string,
+  selectionSet: string[],
+  listSelectionSet: string[]
+) {
   const defaultOptions = {
     //authMode: "userPool",
-    selectionSet
-  }
+    selectionSet,
+  };
   const provider = useNuxtApp().$Amplify.GraphQL.client.models[model];
 
   /**
-  * Call the GraphQL API
-  * @param {string} request - The type of request to perform (e.g., 'create', 'update', 'delete', 'get', 'list').
-  * @param {object} input - The model data to be sent with the request.
-  * @param {object} options - Additional options for the request, such as authentication mode and selection set.
-  * @returns {GraphQLResponse} - A promise that resolves to the response data, which always includes an `id` property.
-  */
-  const call = async (request: string, input:Model|Params, options:Options = {}):Promise<GraphQLResponse> => {
+   * Call the GraphQL API
+   * @param {string} request - The type of request to perform (e.g., 'create', 'update', 'delete', 'get', 'list').
+   * @param {object} input - The model data to be sent with the request.
+   * @param {object} options - Additional options for the request, such as authentication mode and selection set.
+   * @returns {GraphQLResponse} - A promise that resolves to the response data, which always includes an `id` property.
+   */
+  const call = async (
+    request: string,
+    input: Model | Params,
+    options: Options = {}
+  ): Promise<GraphQLResponse> => {
     options = {
       ...defaultOptions,
-      ...options
-    }
+      ...options,
+    };
     const { data } = await provider[request](input, options);
     return data;
-  }
+  };
 
   /**
    * Create a model
@@ -55,9 +62,9 @@
    * @param {object} options the options
    * @returns {Promise<object>}
    */
-  const create = async (input:Model, options:Options = {}) => {
-    return call('create', input, options);
-  }
+  const create = async (input: Model, options: Options = {}) => {
+    return call("create", input, options);
+  };
 
   /**
    * Update a model
@@ -66,9 +73,9 @@
    * @param {object} options the options
    * @returns {Promise<object>}
    */
-  const update = async (input:Model, options:Options = {}) => {
-    return call('update', input, options);
-  }
+  const update = async (input: Model, options: Options = {}) => {
+    return call("update", input, options);
+  };
 
   /**
    * Get a model
@@ -77,9 +84,9 @@
    * @param {object} options the options
    * @returns {Promise<object>}
    */
-  const get = async (id:string, options:Options = {}) => {
-    return call('get', { id }, options);
-  }
+  const get = async (id: string, options: Options = {}) => {
+    return call("get", { id }, options);
+  };
 
   /**
    * Delete a model
@@ -88,14 +95,14 @@
    * @param {object} options the options
    * @returns {Promise<object>}
    */
-  const del = async (model:Model, options:Options = {}) => {
+  const del = async (model: Model, options: Options = {}) => {
     if (!model.id) return;
 
     const { id } = model;
-    const data = await call('delete', { id }, options);
+    const data = await call("delete", { id }, options);
 
     return data;
-  }
+  };
 
   /**
    * List models
@@ -108,13 +115,13 @@
    * @param {object} options the options
    * @returns {Promise<object>}
    */
-  const list = async (params:Params = {}, options:Options = {}) => {
+  const list = async (params: Params = {}, options: Options = {}) => {
     options = {
       selectionSet: listSelectionSet,
-      ...options
-    }
-    return call('list', params, options);
-  }
+      ...options,
+    };
+    return call("list", params, options);
+  };
 
   return {
     call,
@@ -122,6 +129,6 @@
     update,
     get,
     del,
-    list
-  }
+    list,
+  };
 }
