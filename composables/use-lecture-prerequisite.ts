bc338435d8c8d7c265db168c5e1d2217a879d39d
Julien Reichel <julien.reichel@spotme.com>
2024-10-03 17:54:27 +0200

    add definitions

--- composables/use-lecture-prerequisite.ts
+++ composables/use-lecture-prerequisite.ts
@@ -1,3 +1,10 @@
+interface LecturePrerequisiteModel {
+  prerequisiteId: string;
+  lectureId: string;
+  prerequisite?: { id: string } ;
+  followUp?: { id: string } ;
+}
+
 export default function () {
   return useGraphqlQuery('LecturePrerequisite', ['prerequisiteId', 'lectureId']);
 }
--- composables/use-lecture.ts
+++ composables/use-lecture.ts
@@ -1,3 +1,10 @@
+interface LectureModel {
+  id: string;
+  name: { en?: string; fr?: string };
+  prerequisites?: { prerequisite: { id: string } }[];
+  followUps?: { lecture: { id: string } }[];
+}
+
 export default function () {
   return useGraphqlQuery('Lecture', ['id', 'name.*', 'prerequisites.prerequisite.*', 'followUps.lecture.*'], ['id', 'name.*']);
 }
