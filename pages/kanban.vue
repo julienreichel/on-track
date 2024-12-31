f21c665ffc6375755ff209432f3a477a631825f3
Julien Reichel <julien.reichel@spotme.com>
2025-01-01 00:35:09 +0100

    feat: add backlog

--- pages/kanban.vue
+++ pages/kanban.vue
@@ -3,7 +3,12 @@
     <div class="text-h4 q-mb-md">Kanban Board</div>
     <q-card flat bordered class="q-mb-md">
       <q-card-section>
-        <q-spinner v-if="loading" color="primary" size="lg" class="q-my-md text-center" />
+        <q-spinner
+          v-if="loading"
+          color="primary"
+          size="lg"
+          class="q-my-md text-center"
+        />
         <div v-else>
           <!-- Header Row with Column Titles -->
           <div class="row q-col-gutter-md">
@@ -27,8 +32,11 @@
                   flat
                   bordered
                   class="q-mb-sm cursor-pointer"
-                  @click="$router.push(`/competency/${competencyId}`)">
-                  <q-card-section class="text-h5 text-center">{{ competency.name }}</q-card-section>
+                  @click="$router.push(`/competency/${competencyId}`)"
+                >
+                  <q-card-section class="text-h5 text-center">{{
+                    competency.name
+                  }}</q-card-section>
                 </q-card>
               </div>
               <div class="col-10">
@@ -39,36 +47,73 @@
                       :key="concept.id"
                       flat
                       bordered
-                      class="cursor-pointer q-mb-sm" @click="$router.push(`/concept/${concept.id}`)"
-                      >
+                      class="cursor-pointer q-mb-sm"
+                      @click="$router.push(`/concept/${concept.id}`)"
+                    >
                       <q-card-section class="q-pb-xs">
                         {{ concept.name }}
                       </q-card-section>
                       <q-card-section class="q-pt-xs">
                         <div v-if="column === 'ToDo'" class="row">
-                          <q-icon name="cancel" class="cursor-pointer" @click.stop.prevent="deleteConceptAction(concept)" />
+                          <q-icon
+                            name="cancel"
+                            class="cursor-pointer"
+                            @click.stop.prevent="deleteConceptAction(concept)"
+                          />
                         </div>
 
                         <!-- Actions for Learning -->
                         <div v-else-if="column === 'Learning'" class="row">
-                          <q-icon name="article" :color="concept.action.theory ? 'primary': 'grey-4'"/>
-                          <q-icon name="ballot" :color="concept.action.examples ? 'primary': 'grey-4'"/>
-                          <q-icon name="check_box" :color="concept.action.usedFlashCards?.filter(fc => fc.isOk).length >= 3 ? 'primary': 'grey-4'"/>
-                          <q-icon name="help_center" :color="concept.action.answeredQuestions?.filter(q => q.isValid).length >= 8 ? 'primary': 'grey-4'"/>
+                          <q-icon
+                            name="article"
+                            :color="
+                              concept.action.theory ? 'primary' : 'grey-4'
+                            "
+                          />
+                          <q-icon
+                            name="ballot"
+                            :color="
+                              concept.action.examples ? 'primary' : 'grey-4'
+                            "
+                          />
+                          <q-icon
+                            name="check_box"
+                            :color="
+                              concept.action.usedFlashCards?.filter(
+                                (fc) => fc.isOk
+                              ).length >= 3
+                                ? 'primary'
+                                : 'grey-4'
+                            "
+                          />
+                          <q-icon
+                            name="help_center"
+                            :color="
+                              concept.action.answeredQuestions?.filter(
+                                (q) => q.isValid
+                              ).length >= 8
+                                ? 'primary'
+                                : 'grey-4'
+                            "
+                          />
                         </div>
 
                         <!-- Actions for Revision -->
                         <div v-else-if="column === 'Revision'" class="row">
-                            <q-icon
-                              v-for="i in 5"
-                              :key="i"
-                              :name="getBatteryIcon(concept.action, i)"
-                            />
+                          <q-icon
+                            v-for="i in 5"
+                            :key="i"
+                            :name="getBatteryIcon(concept.action, i)"
+                          />
                         </div>
 
                         <!-- Actions for Done -->
                         <div v-else-if="column === 'Done'" class="row">
-                          <q-icon name="visibility_off" class="cursor-pointer" @click.stop.prevent="hideTask(concept)" />
+                          <q-icon
+                            name="visibility_off"
+                            class="cursor-pointer"
+                            @click.stop.prevent="hideTask(concept)"
+                          />
                         </div>
                       </q-card-section>
                     </q-card>
@@ -80,6 +125,35 @@
         </div>
       </q-card-section>
     </q-card>
+
+    <!-- Follow-Up Concepts Section -->
+    <div v-if="!loading" class="q-my-md" >
+      <div class="text-h5 q-mb-md">Backlog</div>
+      <q-card flat bordered>
+        <q-card-section>
+          <div class="row q-col-gutter-md">
+            <div
+              v-for="concept in followUpConcepts"
+              :key="concept.id"
+              class="col-2"
+            >
+              <q-card flat bordered>
+                <q-card-section class="q-pb-xs">
+                  {{ concept.name }}
+                </q-card-section>
+                <q-card-section class="q-pt-xs">
+                  <q-icon
+                    name="add_circle"
+                    class="cursor-pointer"
+                    @click="addFollowUpConcept(concept)"
+                  />
+                </q-card-section>
+              </q-card>
+            </div>
+          </div>
+        </q-card-section>
+      </q-card>
+    </div>
   </div>
 </template>
 
@@ -88,6 +162,7 @@
 const board = ref({});
 const columns = ["ToDo", "Learning", "Revision", "Done"];
 const loading = ref(true);
+const followUpConcepts = ref([]);
 
 const conceptActionService = useConceptAction();
 const conceptService = useConcept();
@@ -97,6 +172,7 @@ const deleteConceptAction = async (concept) => {
     await conceptActionService.delete(concept.action);
     const columns = board.value[concept.competency.id].columns
     columns.ToDo = columns.ToDo.filter(c => c.id !== concept.id);
+    followUpConcepts.value.push(concept);
   } catch (error) {
     console.error("Error deleting concept action:", error);
   }
@@ -127,6 +203,29 @@ const getBatteryIcon = (action, index) => {
   return "battery_0_bar";
 };
 
+const addFollowUpConcept = async (concept) => {
+  try {
+    await conceptActionService.create({
+      conceptId: concept.id,
+      inProgress: true,
+    });
+    followUpConcepts.value = followUpConcepts.value.filter(c => c.id !== concept.id);
+    concept = await conceptService.get(concept.id);
+    existingConceptIds.add(concept.id);
+    concept.action = { inProgress: true };
+    board.value[concept.competency.id].columns.ToDo.push(concept);
+    for (const followUp of concept.followUps) {
+      if (!existingConceptIds.has(followUp.concept.id) && !followUpConcepts.value.some(c => c.id === followUp.concept.id)){
+        followUpConcepts.value.push(followUp.concept);
+      }
+    }
+  } catch (error) {
+    console.error("Error adding follow-up concept:", error);
+  }
+};
+
+const existingConceptIds = new Set();
+
 const fetchKanbanData = async () => {
   try {
     const { getCurrentUser } = useNuxtApp().$Amplify.Auth;
@@ -139,13 +238,13 @@ const fetchKanbanData = async () => {
     });
 
     const groupedByCompetency = {};
-
     for (const action of actions) {
       if (action.actionTimestamps?.some(ts => ts.actionType === 'hidden')) continue;
 
       const concept = await conceptService.get(action.conceptId);
       concept.action = action;
       const competency = concept.competency;
+      existingConceptIds.add(concept.id);
 
       if (!groupedByCompetency[competency.id]) {
         groupedByCompetency[competency.id] = {
@@ -174,7 +273,15 @@ const fetchKanbanData = async () => {
       } else {
         groupedByCompetency[competency.id].columns.Done.push(concept);
       }
+
+      // Collect follow-up concepts
+      for (const followUp of concept.followUps) {
+        if (!followUpConcepts.value.some(c => c.id === followUp.concept.id)) {
+          followUpConcepts.value.push(followUp.concept);
+        }
+      }
     }
+    followUpConcepts.value = followUpConcepts.value.filter(c => !existingConceptIds.has(c.id));
 
     board.value = groupedByCompetency;
   } catch (error) {
